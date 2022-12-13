369cf7c8f382ab6bda02b55a95875f76acd7ff09
Lou CHANCRIN <lchancri@e3r2p10.clusters.42paris.fr>
2022-12-13 15:59:48 +0100

    WIP p2

--- p1/script_server.sh
+++ p1/script_server.sh
@@ -4,8 +4,9 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.19.5+k3s1 K3S_KUBECONFIG_M
 # curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=latest sh -
 echo "**** End installing k3s"
 mkdir /home/vagrant/kuberconfig
+cat /etc/rancher/k3s/k3s.yaml
 sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/kuberconfig
 # chmod +x k3s
 # sudo mv k3s /usr/local/bin
-k3s server
+# k3s server
 # sed -i "s/127\.0\.0\.1/192.168.33.10/" /vagrant/kuberconfig
